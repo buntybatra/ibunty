3babfabb2fd38d5ab2a3232a138e6125efe92f34
bbadgujar <Bunny0503@>
2018-06-19 16:08:36 +0530

    project template changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { RouteRoutingModule, routingComponents } from './route-routing.module';
 import { AppComponent } from './app.component';
@@ -16,9 +18,17 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    RouteRoutingModule
+    RouteRoutingModule,
+    HttpClientModule
   ],
-  providers: [HeaderComponent, FooterComponent],
+  providers: [
+    HeaderComponent,
+    FooterComponent,
+    HttpClient,
+    HttpClientModule,
+    HttpModule
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../servies/http.service'; 
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import {any} from "codelyzer/util/function";
 
 @Component({
   selector: 'app-project-detail',
-  templateUrl: './project-detail.component.html',,
-  providers: [HttpService]
+  templateUrl: './project-detail.component.html',
 })
 export class ProjectDetailComponent implements OnInit {
 
-  public projectinfo: any;
-  constructor ( private getProjectDetails: HttpService ) {}
-
-  public ProjectDetails(){
-  	this.getProjectDetails.projectDetailsdata().subscribe(res => this.projectinfo = res.json());
+  constructor(private http: HttpClient){
   }
 
+  project_title = 'hello';
+  public project_obj = any;
+
+  ngOnInit(): void {
+    this.http.get('https://profile.ibunty.com/project/2?_format=json').subscribe(data => {
+      this.project_obj = data[0];
+      console.log(data[0]);
+
+    });
+  }
 
 }
